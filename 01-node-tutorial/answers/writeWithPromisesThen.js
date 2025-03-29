76c662d83e9a0c2adbb0867ade29b1bd9acd0ee1
AnnaBazileeva <ann.bazileeva@gmail.com>
2025-03-29 19:00:42 -0400

    Async Patterns

---
+++ 01-node-tutorial/answers/writeWithPromisesThen.js
@@ -0,0 +1,13 @@
+const { writeFile, readFile } = require("fs").promises;
+
+function writer() {
+    return writeFile("./temp.txt", "Here is line 1\n")
+        .then(() => writeFile("./temp.txt", "Here is line 2\n", {flag: "a"}))
+        .then(() => writeFile("./temp.txt", "Here is line 3\n", {flag: "a"}))
+        .then(() => console.log("All lines written to temp.txt successfully."))
+        .then(() => readFile("./temp.txt", "utf8"))
+        .then(() => console.log("Content:", data))
+        .catch((error) => {
+            console.error("Error writing to file:", error);
+        })
+}
