e2199b918f06f0c2c9589eb51dc17027c85829f9
AnnaBazileeva <ann.bazileeva@gmail.com>
2025-04-22 17:26:42 -0400

    add methods

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,30 @@
-console.log('Task Manager App')
+const exprees = require('express')
+const app = exprees();
+
+app.get('/hello', (req,res)=> {
+    res.send('Task manager')
+})
+
+app.get('/api/v1/tasks', (req,res)=> {
+    res.send('Task manager')
+})
+
+app.post('/api/v1/tasks', (req,res)=> {
+    res.send('Task manager')
+})
+
+app.get('/api/v1/tasks/:id', (req,res)=> {
+    res.send('Task manager')
+})
+
+app.patch('/api/v1/tasks/:id', (req,res)=> {
+    res.send('Task manager')
+})
+
+app.delete('/api/v1/tasks/:id', (req,res)=> {
+    res.send('Task manager')
+})
+
+const port =3000
+
+app.listen(port, console.log(`server is lictening on port ${port}...`))
