970268ba780b5be3a380c86680d88f2a45e0a99f
AnnaBazileeva <ann.bazileeva@gmail.com>
2025-04-25 11:20:16 -0400

    add custom errors

--- 03-task-manager/starter/controllers/task.js
+++ 03-task-manager/starter/controllers/task.js
@@ -1,26 +1,48 @@
-const Task = require('../models/Task')
+const Task = require("../models/Task");
 const asyncWrapper = require("../middleware/asyncWrapper");
 const { createCustomError } = require("../errors/customError");
-const getAllTasks =  (req, res) => {
-    res.send('get all tasks')
-}
 
-const createTask =  async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(200).json({task})
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+    const tasks = await Task.find({});
+    res
+        .status(200)
+        .json({ status: "success", data: { tasks, nbHits: tasks.length } });
+});
 
-const getTask =  (req, res) => {
-    res.json({id: req.params.id})
-}
+const createTask = asyncWrapper(async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(201).json({ status: "success", data: { task } });
+});
 
-const deleteTask = (req, res) => {
-    res.send('delete task')
-}
+const getTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+        return next(createCustomError(`Task with id : ${taskID} not found`, 404));
+    }
+    res.status(200).json({ status: "success", data: { task } });
+});
 
-const updateTask = (req, res) => {
-    res.send('update task')
-}
+const deleteTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+        return next(createCustomError(`Task with id : ${taskID} not found`, 404));
+    }
+    res.status(200).json({ status: "success", data: { task } });
+});
+
+const updateTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+        new: true,
+        runValidators: true,
+    });
+    if (!task) {
+        return next(createCustomError(`Task with id : ${taskID} not found`, 404));
+    }
+    res.status(200).json({ status: "success", data: { task } });
+});
 
 module.exports = {
     getAllTasks,
@@ -28,4 +50,4 @@ module.exports = {
     getTask,
     updateTask,
     deleteTask,
-}
+};
--- 03-task-manager/starter/errors/customError.js
+++ 03-task-manager/starter/errors/customError.js
@@ -5,8 +5,8 @@ class CustomAPIError extends Error {
     }
 }
 
-const createCustomError = (message, statusCode) => {
-    return new CustomAPIError(message, statusCode);
+const createCustomError = (msg, statusCode) => {
+    return new CustomAPIError(msg, statusCode);
 };
 
 module.exports = { createCustomError, CustomAPIError };
