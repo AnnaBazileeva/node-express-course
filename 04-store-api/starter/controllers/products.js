2312030bec3d8f3aff27a81c44eefac55abb56be
AnnaBazileeva <ann.bazileeva@gmail.com>
2025-05-01 22:45:15 -0400

    add sort

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,11 +1,11 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await  Product.find('name: "entertainment center"')
+    const products = await  Product.find({}).sort('name price')
     res.status(200).json({products, nbHits: products.length})
 }
 const getAllProducts= async (req, res) => {
-    const {featured,company, name, price, _id, rating} =req.query
+    const {featured,company, name, price, _id, rating, sort} =req.query
     const queryObject = {}
 
     if (featured) {
@@ -15,19 +15,27 @@ const getAllProducts= async (req, res) => {
         queryObject.company = company
     }
     if(name) {
-        queryObject.name = name
+        queryObject.name = { $regex:name, $options: 'i'}
     }
     if(price) {
         queryObject.price = price
     }
-    if(name) {
+    if(rating) {
         queryObject.rating = rating
     }
     if(_id) {
-        queryObject.name = _id
+        queryObject._id = _id
     }
     console.log(queryObject)
-    const products = await  Product.find(req.query)
+
+    let result = Product.find(queryObject)
+    if(sort) {
+      const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+    const products = await result
     res.status(200).json({products, nbHits: products.length})
 }
 
