25678814c5ca284d1d3614f29a82e92527294e21
AnnaBazileeva <anna.bazileeva@gmail.com>
2025-05-10 01:18:44 -0400

    add token

--- controllers/main.js
+++ controllers/main.js
@@ -16,19 +16,24 @@ const login = async (req, res)=> {
 
 const dashboard = async (req, res) => {
     const authHeader = req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith('Bearer')) {
-        throw new CustomAPIError('No token provided', 401)
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new CustomAPIError('No token provided', 401);
     }
 
-    const token = authHeader.split('')[1]
-
+    const token = authHeader.split(' ')[1]
+    console.log('Received token:', token);
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    }catch (error) {
-throw new CustomAPIError('Not authorized to access this route', 401)
+        console.log('Decoded token:', decoded);
+        const luckyNumber = Math.floor(Math.random() * 100)
+        res.status(200).json({
+            msg: `Hello, ${decoded.username}`,
+            secret: `Here is your authorized data, you lucky number is ${luckyNumber}`
+        })
+    } catch (error) {
+        console.error('JWT Error:', error);
+        throw new CustomAPIError('Not authorized to access this route', 401)
     }
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello, John`, secret: `Here is your authorized data, you lucky number is ${luckyNumber}`})
 }
 
 module.exports = {
